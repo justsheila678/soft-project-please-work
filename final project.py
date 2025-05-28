e19f1dd55465d894d0cc09f78bf01135bb5582f6
justsheila678 <smercado2468@gmail.com>
2025-05-28 12:20:03 -0400

    fixed problems with indents, punto y coma and small errors

--- final project.py
+++ final project.py
@@ -10,31 +10,31 @@ class PresupuestoApp:
         self.page.vertical_alignment = "start"
         self.main_view()
    
-   def main_view(self, e=None):
+        def main_view(self, e=None):
+            self.page.controls.clear()
+            titulo = ft.Text("Menú Principal", size=30, weight="bold")
+            btn_nuevo = ft.ElevatedButton(("Agregar nuevo Presupuesto"), on_click=self.nuevo_presupuesto_view)
+            btn_lista = ft.ElevatedButton ("Listar Presupuestos", on_click=self.listar_presupuestos_view)
+            self.page.add(titulo,btn_nuevo, btn_lista)
+            self.page.update()
+
+    def nuevo_presupuesto_view(self, e):
         self.page.controls.clear()
-        titulo = ft.Text("Menú Principal", size=30, weight="bold")
-        btn_nuevo = ft.ElevatedButton(("Agregar nuevo Presupuesto"), on_click=self.nuevo_presupuesto_view)
-        btn_lista = ft.ElevatedButton ("Listar Presupuestos", on_click=self.listar_presupuestos_view)
-        self.page.add(titulo,btn_nuevo, btn_lista)
+
+        nombre = ft.TextField(label="Nombre del presupuesto")
+        balance = ft.TextField(label="Balance inicial", keyboard_type=ft.KeyboardType.NUMBER)
+        def crear_presupuesto(ev):
+            if nombre.value and balance.value:
+                self.main_view(None)
+        self.page.add(
+            ft.Text("Nuevo Presupuesto", size=25, weight="bold"),
+            nombre,
+            balance,
+            ft.Row([
+                ft.ElevatedButton("Crear, on_click=crear_presupuesto"),
+                ft.OutlinedButton("Cancelar", on_click=self.main_view)
+
+            ])
+        )
         self.page.update()
-
-  def nuevo_presupuesto_view(self, e);
-      self.page.controls.clear()
-
-      nombre = ft.TextField(label="Nombre del presupuesto")
-      balance = ft.TextField(label="Balance inicial", keyboard_type=ft.KeyboardType.NUMBER)
-      def crear_presupuesto(ev):
-      if nombre.value and balance.value;
-        self.main_view(None)
-      self.page.add(
-        ft.Text("Nuevo Presupuesto", size=25, weight="bold"),
-          nombre,
-          balance,
-          ft.Row([
-              ft.ElevatedButton("Crear, on_click=crear_presupuesto),
-              ft.OutlinedButton(Cancerlar, on_click=self.main_view)
-
-          ])
-      )
-      self.page.update()
 
